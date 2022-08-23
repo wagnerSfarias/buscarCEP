c661312d4a74af7c9de2e338a68806eea6f4bd71
wagnerSfarias <wagner@universalhost.com.br>
2022-08-22 21:56:37 -0300

    update text

--- App.js
+++ App.js
@@ -133,7 +133,6 @@ export default function App() {
         <Text style={styles.resultText}>Cidade: {cepUser.localidade}</Text>
         <Text style={styles.resultText}>UF: {cepUser.uf}</Text>
        </View>
-      
       }
     
       </ImageBackground> 
